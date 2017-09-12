f18160b932b5302556f2c122a91857fe9a403fef
estrangegit <etienne.estrangin@gmail.com>
2017-09-12 10:14:02 +0200

    Réalisation du modèle de données pour l'affichage

---
+++ src/app/client-container/dessert/dessert.ts
@@ -0,0 +1,8 @@
+export class Dessert {
+  constructor(
+    public title: string,
+    public description: string,
+    public price: number,
+    public category: string
+  ) {  }
+}
---
+++ src/app/client-container/drink/drink.ts
@@ -0,0 +1,8 @@
+export class Drink {
+  constructor(
+    public title: string,
+    public description: string,
+    public price: number,
+    public category: string
+  ) {  }
+}
