58c7f1995e35a7d68c45411e0a8fea7d2c265339
hamzahamidi <hamza.hamidi@insa-cvl.fr>
2017-09-18 09:14:39 +0200

    Add function add to cart

--- src/app/client-container/shopping-cart/shopping-cart-data.service.ts
+++ src/app/client-container/shopping-cart/shopping-cart-data.service.ts
@@ -1,61 +1,69 @@
 import { Injectable } from '@angular/core';
-import {Item} from '../item/model';
+import { Item } from '../item/model';
 
 @Injectable()
 export class ShoppingCartDataService {
 
-  private shoppingItems: {item: Item, quantity: number}[];
+  private shoppingItems: { item: Item, quantity: number }[];
 
   constructor() {
     this.shoppingItems = [];
   }
 
-  public getShoppingItems(){
+  public getShoppingItems() {
     return this.shoppingItems;
   }
 
-  public setShoppingItems(shoppingItems: any[]){
+  public setShoppingItems(shoppingItems: any[]) {
     this.shoppingItems = shoppingItems;
   }
+  public addToCart(shoppingItems: any) {
+    const index = this.shoppingItems.findIndex(_item => _item.item.title.indexOf(shoppingItems.item.title) > -1);
+    if (index > -1) {
+      this.shoppingItems[index].quantity += shoppingItems.quantity;
+    }
+    else this.shoppingItems.push(shoppingItems);
 
-  public addShoppingItem(item: Item, quantity: number = 1){
+  }
+
+  public addShoppingItem(item: Item, quantity: number = 1) {
 
     let index = -1;
 
-    for(let i = 0; i < this.shoppingItems.length; i++){
-      if(this.shoppingItems[i].item.id == item.id){
+    for (let i = 0; i < this.shoppingItems.length; i++) {
+      if (this.shoppingItems[i].item.id == item.id) {
         index = i;
         break;
       }
     }
 
-    if(index == -1){
-      this.shoppingItems.push({item: item, quantity: quantity});
-    }else{
+    if (index == -1) {
+      this.shoppingItems.push({ item: item, quantity: quantity });
+    } else {
       this.shoppingItems[index].quantity += quantity;
     }
   }
 
-  public removeShoppingItem(id: number){
+  public removeShoppingItem(id: number) {
     let index = -1;
 
-    for(let i = 0; i < this.shoppingItems.length; i++){
-      if(this.shoppingItems[i].item.id == id){
+    for (let i = 0; i < this.shoppingItems.length; i++) {
+      if (this.shoppingItems[i].item.id == id) {
         index = i;
         break;
       }
     }
 
-    if(index >= 0){
+    if (index >= 0) {
       this.shoppingItems.splice(index, 1);
     }
   }
 
-  public totalPrice(){
+  public totalPrice() {
     let total = 0;
 
-    for(let i = 0; i < this.shoppingItems.length; i++){
-      total = this.shoppingItems[i].item.price * this.shoppingItems[i].quantity;
+    for (let i = 0; i < this.shoppingItems.length; i++) {
+      total += this.shoppingItems[i].item.price * this.shoppingItems[i].quantity;
     }
     return total;
   }
