0395300d46b81d21ee0c19884ebbc6abd5706fc3
estrangegit <etienne.estrangin@gmail.com>
2017-09-19 17:16:35 +0200

    Affichage des catégories et des produits par type

--- src/app/shared/gizzaBackEndAPIConfig.ts
+++ src/app/shared/gizzaBackEndAPIConfig.ts
@@ -1,5 +1,7 @@
 export const gizzaBackEndAPIConfig = {
   searchAllCategoriesUrl: '/gizza/api/categorie/all',
   searchAllPizzasUrl: '/gizza/api/pizza/all',
+  searchAllDrinksUrl: '/gizza/api/boisson/all',
+  searchAllDessertsUrl: '/gizza/api/dessert/all',
   searchTestUrl: 'https://geo.api.gouv.fr/departements'
 };
