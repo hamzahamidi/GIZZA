159ca042f78c5ac2f488cda24f2712756480b951
gaurang1589 <Gaurang@eduroam-075172.grenet.fr>
2017-09-21 12:12:32 +0200

    IHM for add category
    Created the IHM for add category functionality

--- src/app/admin-container/admin-category/admin-category/admin-category.component.ts
+++ src/app/admin-container/admin-category/admin-category/admin-category.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ItemService } from '../../../client-container/item/item.service';
+import { Category, Item, PIZZACATEGORIES, PIZZAS, BOISSONS, DESSERTS, TypeItem } from '../../../client-container/item/model';
+
 
 @Component({
   selector: 'app-admin-category',
@@ -7,9 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminCategoryComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+  currentType:string;
+  constructor( private itemService: ItemService) { }
 
   ngOnInit() {
+    this.currentType = "pizzas"
   }
 
+
+  getCategories(type: string){
+    this.currentType = type;
+    this.categories.length = 0;
+    this.itemService.getCategories().subscribe(data => {
+      for(let i = 0; i < data.items.length; i++){
+        if(data.items[i].type == type)
+          this.categories.push(new Category(data.items[i].id, data.items[i].libelle))
+      }
+    });
+    this.categories.push(new Category(0, "Tous"));
+  }
 }
